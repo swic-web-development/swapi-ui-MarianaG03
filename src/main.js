4985590914ac699ad98fb7b9fa8e5025d52c76a8
Mariana Garcia <mariana.garcia@student.swic.edu>
2025-05-04 21:20:50 -0500

    Fetch People Data from SWAPI

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import './styles.css'
+
+async function fetchPeople() {
+  const response = await fetch('https://swapi.dev/api/people/')
+  const data = await response.json()
+  return data.results
+}
